0698bd5b0312bd3511067b1bd10aad4bdc7ad2f1
Ratchapon Ngamdan <ratchapon.ngam@gmail.com>
2023-02-09 13:39:03 +0700

    fixed price function

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 "use strict";
 
 const button = document.querySelector("input");
+const basicPrice = document.querySelector(".basic");
+const proPrice = document.querySelector(".professional");
+const masterPrice = document.querySelector(".master");
+let click = false;
 
 button.addEventListener("click", function () {
-  const basicPrice = document.querySelector(".basic");
-  const proPrice = document.querySelector(".professional");
-  const masterPrice = document.querySelector(".master");
-
-  //   const defaultBasic = basicPrice.innerHTML;
-  //   const defaultPro = proPrice.innerHTML;
-  //   const defaultMaster = masterPrice.innerHTML;
-
-  let click = false;
-
   if (!click) {
     basicPrice.innerHTML = "199.99";
     proPrice.innerHTML = "249.99";
